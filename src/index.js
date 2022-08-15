971ac0eafef7817d96963deb6d5d5203003a5057
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-16 02:23:34 +0500

    Project setup
    Making project functional

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import Navbar from './components/Navbar';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
