a525cab81409ae79333eb4c70a1a5812c08a0356
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-16 02:12:50 +0500

    Project Setup

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,13 @@
+const CHECK_STATUS = 'CHECK_STATUS';
+
+// Reducer
+export default function reducer(state = [], action = {}) {
+  switch (action.type) {
+    case CHECK_STATUS: return 'Under construction';
+    default: return state;
+  }
+}
+
+// Action Creators
+export function checkStatus() {
+  return { type: CHECK_STATUS };
